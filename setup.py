6c0d2a80fdde382456acb3eca35a46bb7ee4ad73
sananth12 <sananthanatarajan12@gmail.com>
2014-06-01 21:31:08 +0530

    upgrading info

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 setup(name='ImageScraper',
-      version='1.0.3',
+      version='1.0.4',
     install_requires=['lxml'],
     author='Anantha Natarajan',
     author_email='sananthanatarajan12@gmail.com',
     packages=['image_scraper'],
     test_suite='tests',
     url='https://github.com/sananth12/ImageScraper/',
-    description='A simple image scraper to download all images from a given url',
+    description='A simple image scraper to download all  images from a given url',
       )
