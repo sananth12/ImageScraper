12d295401c8d318f64462f24d637763f3006ec94
John Osborne <osborne6@gmail.com>
2014-10-02 21:28:50 -0500

    sorta working as a command line app with setuptools entry_points

--- image_scraper/__init__.py
+++ image_scraper/__init__.py
@@ -5,6 +5,7 @@ import requests
 import urlparse
 import os
 from progressbar import *
+import argparse
 
 def process_links(links):
     x=[]
@@ -13,65 +14,79 @@ def process_links(links):
                 x.append(l)
     return x
  
-URL = raw_input("Enter URL to scrap: ")
+def console_main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('url2scrape', nargs=1, help="URL to scrape")
+    parser.add_argument('--max-images', type=int, default=1, help="URL to scrape")
+    args = parser.parse_args()
 
-page = requests.get(URL)
+    #URL = raw_input("Enter URL to scrap: ")
+    URL = args.url2scrape[0]
 
-tree = html.fromstring(page.text)
-#img = tree.xpath('//img/@src')
-img = tree.xpath('//img/@src')
+    page = requests.get(URL)
 
-links = tree.xpath('//a/@href')
+    tree = html.fromstring(page.text)
+    #img = tree.xpath('//img/@src')
+    img = tree.xpath('//img/@src')
 
-img_links =  process_links(links)
+    links = tree.xpath('//a/@href')
 
-##sub_img = tree.xpath('//descendant-or-self::*[img]/img/@src') 
+    img_links =  process_links(links)
 
-img.extend(img_links)
+    ##sub_img = tree.xpath('//descendant-or-self::*[img]/img/@src') 
 
-if len(img)==0:
-    sys.exit( "Sorry, no images found")
+    img.extend(img_links)
 
-print "Found %s images: "%len(img)
+    if len(img)==0:
+        sys.exit( "Sorry, no images found")
 
-no_to_download = int(input('How many images do you want ? : '))
+    print "Found %s images: "%len(img)
 
-images = [urlparse.urljoin(page.url, url) for url in img] 
+    #no_to_download = int(input('How many images do you want ? : '))
+    no_to_download = args.max_images
 
-for x  in range(0,len(img)):
-    if  img[x][:4]!="http":
-        img[x]="https:"+img[x]
+    images = [urlparse.urljoin(page.url, url) for url in img] 
+    print img
+    print images
+    # this does not work if the urls are relative
+    for x  in range(0,len(img)):
+        if  img[x][:4]!="http":
+            img[x]="https:"+img[x]
 
-if not os.path.exists('images'):
-    os.makedirs('images')
+    if not os.path.exists('images'):
+        os.makedirs('images')
 
-count=0
-bar="["
-percent=0.0
-failed=0
-widgets = ['Progress: ', Percentage(), ' ', Bar(marker=RotatingMarker()),' ', ETA(), ' ', FileTransferSpeed()]
-pbar = ProgressBar(widgets=widgets, maxval=100).start()
+    count=0
+    bar="["
+    percent=0.0
+    failed=0
+    widgets = ['Progress: ', Percentage(), ' ', Bar(marker=RotatingMarker()),' ', ETA(), ' ', FileTransferSpeed()]
+    pbar = ProgressBar(widgets=widgets, maxval=100).start()
 
 
-for img_url in img :
-    try:
-    	tmp = requests.request('get',img_url)
-    	f = open('images/%s' % img_url.split('/')[-1], 'w')
-    	f.write( tmp.content)
-    	f.close()    
-	count+=1
-        percent = percent + 100.0/len(img)
-        pbar.update( percent )
-    
-    except:
-        failed +=1
-	#print "Can't download %s"%img_url
-    if count==no_to_download:
-        	break
+    print img
+    #for img_url in img :
+    for img_url in images :
+        try:
+            tmp = requests.request('get',img_url)
+            print "status : %s" % tmp.status_code
+            f = open('images/%s' % img_url.split('/')[-1], 'w')
+            f.write( tmp.content)
+            f.close()    
+            count+=1
+            percent = percent + 100.0/len(img)
+            pbar.update( percent )
+        
+        except:
+            failed +=1
+            #print "Can't download %s"%img_url
+        if count==no_to_download:
+                    break
 
-pbar.finish()
-print 
-print "Done. Failed to download %s images"%failed
+    pbar.finish()
+    print 
+    print "Done. Failed to download %s images"%failed
+    return
 
 
 	
--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup
 setup(name='ImageScraper',
       version='1.1.0',
     install_requires=['lxml'],
     author='Anantha Natarajan',
     author_email='sananthanatarajan12@gmail.com',
     packages=['image_scraper'],
+    entry_points = {
+        'console_scripts': ['image-scraper=image_scraper:console_main'],
+        },
     test_suite='tests',
     url='https://github.com/sananth12/ImageScraper/',
     description='A simple image scraper to download all  images from a given url',
