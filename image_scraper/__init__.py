bae529b6bd569acaafeb0ca61916fe2f5c42db52
Anantha Natarajan <sananthanatarajan12@gmail.com>
2015-01-17 22:58:01 +0530

    clean up

--- image_scraper/__init__.py
+++ image_scraper/__init__.py
@@ -18,6 +18,7 @@ def process_links(links):
 
 
 def console_main():
+
     parser = argparse.ArgumentParser()
     parser.add_argument('url2scrape', nargs=1, help="URL to scrape")
     parser.add_argument('--max-images', type=int, default=1,
@@ -26,6 +27,7 @@ def console_main():
                         help="Directory in which images should be saved")
     parser.add_argument('-g', '--injected', help="scrape injected images",
                         action="store_true")
+
     args = parser.parse_args()
     
     URL = args.url2scrape[0]
@@ -33,6 +35,7 @@ def console_main():
     no_to_download = args.max_images
 
     save_dir = args.save_dir
+
     download_path = os.path.join(os.getcwd(), save_dir)
 
     use_ghost = args.injected
@@ -53,7 +56,7 @@ def console_main():
             page_html= page.text
             page_url = page.url
         except requests.exceptions.MissingSchema:
-            URL = "http://" + URL #Default schema is HTTP unless specified
+            URL = "http://" + URL 
             page = requests.get(URL)
             page_html= page.text
             page_url = page.url
@@ -62,8 +65,6 @@ def console_main():
     img = tree.xpath('//img/@src')
     links = tree.xpath('//a/@href')
     img_links = process_links(links)
-
-    # sub_img = tree.xpath('//descendant-or-self::*[img]/img/@src')
 
     img.extend(img_links)
 
@@ -76,8 +77,6 @@ def console_main():
 
     images = [urlparse.urljoin(page_url, url) for url in img]
 
-    #print img
-    #print images
     # this does not work if the urls are relative
     for x in range(0, len(img)):
         if img[x][:4] != "http":
@@ -86,10 +85,8 @@ def console_main():
     #Checking if the path exists
     if os.path.exists(download_path):
         if not os.access(os.path.dirname(download_path), os.W_OK):
-            #path exists but no write permissions
             sys.exit("Sorry, the directory can't be accessed.")
     elif os.access(os.path.dirname(download_path), os.W_OK):
-        #if write permissions are availible, create the directory
         os.makedirs(download_path)
     else:
         sys.exit("Sorry, the directory can't be created.")
@@ -101,8 +98,6 @@ def console_main():
                ' ', ETA(), ' ', FileTransferSpeed()]
     pbar = ProgressBar(widgets=widgets, maxval=100).start()
 
-    #print img
-    # for img_url in img :
     for img_url in images:
         img_request = None
         try:
@@ -111,7 +106,6 @@ def console_main():
             failed += 1
             print "download of %s failed; status code %s" % \
                   (img_url, img_request.status_code)
-            # print "Can't download %s"%img_url
             print "status : %s" % img_request.status_code
         f = open(os.path.join(download_path,  img_url.split('/')[-1]), 'w')
         f.write(img_request.content)
