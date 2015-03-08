83a5ddf2441e5443e899ef114cd0d8736574d18c
Sriram Sundarraj <sriram.s.1994@gmail.com>
2015-03-08 13:04:30 +0530

    Added tests for get_html function.

--- tests/test.py
+++ tests/test.py
@@ -6,3 +6,13 @@ from nose.tools import eq_
 def test_sum():
     eq_(2+2,4)
 
+def test_get_html_200():
+	import requests
+	r=requests.get('http://ananth.co.in/test.html')
+	actual_html=u'<html>\n\n<head>\n    \n</head>\n\n<body>\n<img src="images/test1.jpg"/>\n<img src="images/test.png"/>\n<img src="images/test4.gif"/>\n</body>\n    \n</html>\n'
+	eq_(r.text, actual_html)
+
+def test_get_html_404():
+	import requests
+	r=requests.get('http://ananth.co.in/test404.html')
+	eq_(r.status_code, 404)
