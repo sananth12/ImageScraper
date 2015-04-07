6612fe6f2bf57106e7daa2750d694270d5cbacaa
Sriram Sundarraj <sriram.s.1994@gmail.com>
2015-04-07 15:37:03 +0530

    :bug: Fix with new thread pool.

--- image_scraper/mains.py
+++ image_scraper/mains.py
@@ -49,12 +49,12 @@ def console_main():
     widgets = ['Progress: ', Percentage(), ' ', Bar(marker=RotatingMarker()),
                ' ', ETA(), ' ', FileTransferSpeed()]
     pbar = ProgressBar(widgets=widgets, maxval=100).start()
-    pool = SimplePool.threadpool()
+    pool = SimplePool.ThreadPool()
     status_lock = threading.Lock()
     for img_url in scraper.images:
         if status_flags['count'] == scraper.no_to_download:
             break
-        download_job = SimplePool.thread_job(download_worker_fn, (scraper, img_url, pbar, status_flags, status_lock))
+        download_job = SimplePool.ThreadJob(download_worker_fn, (scraper, img_url, pbar, status_flags, status_lock))
         pool.add_job(download_job)
         status_flags['count'] += 1
 
