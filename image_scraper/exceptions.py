1f3ef44d6d6ccdf06887e6f27e5f013d7e333fbb
Sriram Sundarraj <sriram.s.1994@gmail.com>
2015-08-14 18:53:15 +0530

    Added base class constructor calls.

--- image_scraper/exceptions.py
+++ image_scraper/exceptions.py
@@ -1,27 +1,37 @@
+""" Contains all the custom exceptions used. """
+
 class DirectoryAccessError(Exception):
+    """ Exception to be raised when the directory can't be accessed. """
     pass
 
 
 class DirectoryCreateError(Exception):
+    """ Exception to be raised when the directory can't be created. """
     pass
 
 
 class ImageDownloadError(Exception):
+    """ Exception to be raised when the imace can't be downloaded. """
     status_code = 0
 
     def __init__(self, status_code=0):
+        super(ImageDownloadError, self).__init__()
         self.status_code = status_code
 
 
 class ImageSizeError(Exception):
+    """ Exception to be raised when the image is over the file size. """
     image_size = 0
 
     def __init__(self, image_size):
+        super(ImageSizeError, self).__init__()
         self.image_size = image_size
 
 
 class PageLoadError(Exception):
+    """ Exception to be raised when the page can't be loaded. """
     status_code = 0
 
     def __init__(self, status_code):
+        super(PageLoadError, self).__init__()
         self.status_code = status_code
